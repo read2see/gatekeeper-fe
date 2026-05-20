973bb7f4e538992b61bcbf7ad5a3df456ec42225
Husain Habib <habib.h.e@outlook.com>
2026-05-20 07:48:20 +0300

    chore: add auth dependencies and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-auth-utils'],
 
   devtools: {
     enabled: true
@@ -11,11 +8,30 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  icon: {
+    serverBundle: 'local',
+    clientBundle: {
+      scan: true,
+      icons: ['lucide:sun', 'lucide:moon']
+    }
+  },
+
+  runtimeConfig: {
+    gatekeeperApiBase: 'http://localhost:8080',
+    public: {
+      gatekeeperApiBase: 'http://localhost:8080'
+    }
+  },
+
   routeRules: {
     '/': { prerender: true }
   },
 
   compatibilityDate: '2025-01-15',
+
+  auth: {
+    loadStrategy: 'server-first'
+  },
 
   eslint: {
     config: {
